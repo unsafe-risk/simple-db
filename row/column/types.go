c4eabe1a36408fc3eb5755e850ee9a39b3c57ee0
snowmerak <snowmerak@outlook.com>
2022-12-08 03:13:27 +0900

    write column types

---
+++ row/column/types.go
@@ -0,0 +1,25 @@
+package column
+
+type Column interface {
+	[]byte | string | float64 | float32 | int64 | int32 | int16 | int8 | uint64 | uint32 | uint16 | uint8 | bool
+}
+
+type Changable interface {
+	float64 | float32 | int64 | int32 | int16 | int8 | uint64 | uint32 | uint16 | uint8 | bool
+}
+
+const (
+	Bytes = iota
+	String
+	Float64
+	Float32
+	Int64
+	Int32
+	Int16
+	Int8
+	Uint64
+	Uint32
+	Uint16
+	Uint8
+	Bool
+)
