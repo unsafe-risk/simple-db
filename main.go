ef2bf3b0dd06e6624a6674dfdeffa73382bc24cc
snowmerak <snowmerak@outlook.com>
2022-12-09 15:28:06 +0900

    update sample code

--- main.go
+++ main.go
@@ -3,29 +3,53 @@ package main
 import (
 	"fmt"
 
-	"github.com/unsafe-risk/simple-db/buffer"
 	"github.com/unsafe-risk/simple-db/row"
 	"github.com/unsafe-risk/simple-db/row/column"
+	"github.com/unsafe-risk/simple-db/table"
 )
 
 func main() {
-	wb := buffer.NewWriteBuffer()
-	wb.WriteString("John")
-	wb.WriteInt8(20)
-	wb.WriteInt8(120)
+	persons, err := table.New("datas/persons")
+	if err != nil {
+		panic(err)
+	}
 
-	r := row.NewRow(column.String, column.Int8, column.Int8)
-	r.SetBytes(wb.Bytes())
-	if err := row.Modify(r, 0, "merak"); err != nil {
+	persons.SetColumns(column.String, column.Int8)
+
+	r, err := persons.GetBlankRow()
+	if err != nil {
 		panic(err)
 	}
-	if err := row.Modify[int8](r, 1, 27); err != nil {
+
+	row.Modify(r, 0, "John Doe")
+	row.Modify(r, 1, int8(42))
+
+	if err := persons.SetRow([]byte("1"), r); err != nil {
 		panic(err)
 	}
-	if err := row.Modify[int8](r, 2, 100); err != nil {
+
+	r, err = persons.GetRow([]byte("1"))
+	if err != nil {
 		panic(err)
 	}
-	fmt.Println(row.Get[string](r, 0))
-	fmt.Println(row.Get[int8](r, 1))
-	fmt.Println(row.Get[int8](r, 2))
+
+	name, err := row.Get[string](r, 0)
+	if err != nil {
+		panic(err)
+	}
+
+	age, err := row.Get[int8](r, 1)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(name, age)
+
+	if err := persons.DeleteRow([]byte("1")); err != nil {
+		panic(err)
+	}
+
+	if err := persons.Close(); err != nil {
+		panic(err)
+	}
 }
