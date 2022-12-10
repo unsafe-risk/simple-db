291d98e84facd4658b733a77d0e905b09511c4ed
snowmerak <snowmerak@outlook.com>
2022-12-10 22:36:41 +0900

    update sample code

--- main.go
+++ main.go
@@ -2,68 +2,25 @@ package main
 
 import (
 	"fmt"
-	"os"
-	"os/signal"
-
-	"github.com/unsafe-risk/simple-db/row"
-	"github.com/unsafe-risk/simple-db/row/column"
-	"github.com/unsafe-risk/simple-db/table"
+	"github.com/unsafe-risk/simple-db/buffer/column"
+	"github.com/unsafe-risk/simple-db/query/executor"
 )
 
 func main() {
-	persons, err := table.New("datas/persons")
+	persons, err := executor.New()
 	if err != nil {
 		panic(err)
 	}
-
-	persons.SetColumns(column.String, column.Int8)
-
-	if ok := persons.Lock("1"); !ok {
-		panic("lock failed")
-	}
-
-	r, err := persons.GetBlankRow()
-	if err != nil {
+	if err := persons.SetTable("persons", []string{"name", "age"}, []int{column.String, column.Int8}); err != nil {
 		panic(err)
 	}
 
-	row.Modify(r, 0, "John Doe")
-	row.Modify(r, 1, int8(42))
-
-	if err := persons.SetRow("1", r); err != nil {
-		panic(err)
-	}
-
-	r, err = persons.GetRow("1")
-	if err != nil {
-		panic(err)
-	}
-
-	name, err := row.Get[string](r, 0)
-	if err != nil {
-		panic(err)
-	}
-
-	age, err := row.Get[int8](r, 1)
-	if err != nil {
-		panic(err)
-	}
-
-	fmt.Println(name, age)
-
-	if err := persons.DeleteRow("1"); err != nil {
-		panic(err)
-	}
-
-	if ok := persons.Unlock("1"); !ok {
-		panic("unlock failed")
-	}
+	fmt.Println(persons.Run([]byte("set 1 name snowmerak")))
+	fmt.Println(persons.Run([]byte("set 1 age 21")))
+	fmt.Println(persons.Run([]byte("get 1 name")))
+	fmt.Println(persons.Run([]byte("get 1 age")))
 
 	if err := persons.Close(); err != nil {
 		panic(err)
 	}
-
-	sig := make(chan os.Signal, 1)
-	signal.Notify(sig, os.Interrupt)
-	<-sig
 }
